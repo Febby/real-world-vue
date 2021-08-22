f775ca06f5c6eeca8b01ebe98f2e5682ee12a0f6
febby <gunawan.febby@gmail.com>
2021-08-22 22:01:36 +0700

    Working on route
    - Create a new EventDetails component to display the event’s details
    - Add a new API call to fetch a single event by its id (this is the event we’ll display the details of)
    - Add a route for the new EventDetails component
    - Make EventCard clickable so we can access this new EventDetails route

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,20 +1,20 @@
 <template>
- <div class="event-card">
-   <!-- Display event data -->
-   <span>@ {{ event.time }} on {{ event.date }}</span>
-   <h4>{{ event.title }}</h4>
- </div>
-
+  <router-link to="/event/123">
+    <div class="event-card">
+      <!-- Display event data -->
+      <span>@ {{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>  
 </template>
 
 <script>
 export default {
   name: "EventCard",
   props: {
-   event: Object,
-  }
-
-  }
+    event: Object,
+  },
+};
 </script>
 
 <style scoped>
@@ -22,12 +22,11 @@ export default {
   padding: 20px;
   width: 250px;
   cursor: pointer;
-  border: 1px solid #39495C;
+  border: 1px solid #39495c;
   margin-bottom: 18px;
 }
 .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
-
 </style>
